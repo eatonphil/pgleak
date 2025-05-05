0a855ee631214a24c55be98fb1f6b4b8be9ba4d3
Phil Eaton <phil.eaton@enterprisedb.com>
2025-05-05 16:36:29 -0400

    Setup

--- setup.sql
+++ setup.sql
@@ -3,4 +3,3 @@ CREATE EXTENSION pgleak;
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (id BIGSERIAL, age INT);
 INSERT INTO users (age) SELECT random() * 100 + 1 FROM generate_series(1, 100000000);
--- SELECT pgleak.leak_sum(age, 10) FROM users;

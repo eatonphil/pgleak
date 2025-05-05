543555544763d5fe746bdf7869c6d9a90581527d
Phil Eaton <phil.eaton@enterprisedb.com>
2025-05-05 16:38:04 -0400

    format

--- setup.sql
+++ setup.sql
@@ -2,4 +2,8 @@ DROP EXTENSION IF EXISTS pgleak;
 CREATE EXTENSION pgleak;
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (id BIGSERIAL, age INT);
-INSERT INTO users (age) SELECT random() * 100 + 1 FROM generate_series(1, 100000000);
+INSERT INTO users (age)
+  SELECT
+    random() * 100 + 1
+  FROM
+    generate_series(1, 100000000);
